e0bfa2b4df347b64955aac84e8980e3b17ac06f6
mijan-joy <mmrsm46@gmail.com>
2022-03-23 01:01:23 +0600

    first local stroage

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getStoreCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
+import '../Cart/Cart.css';
 import Product from '../Product/Product';
 import './Shop.css';
 
@@ -9,13 +11,31 @@ const Shop = () => {
     useEffect( () => {
         fetch(`products.json`)
         .then(res=>res.json())
-        .then(data=> setProducts(data))
+        .then(data=> {
+            setProducts(data)
+        })
     }, []);
+
+    useEffect(() => {
+        const storedCart = getStoreCart();
+        const savedCart = [];
+        for (const id in storedCart) {
+            const addedProduct = products.find(product => product.id === id);
+            if(addedProduct) {
+                const quantity = storedCart[id];
+                addedProduct.quantity=quantity;
+                savedCart.push(addedProduct);
+            }
+        }
+        setCart(savedCart);
+    },[products])
+
 
     const handleAddToCart=(product)=>{
         console.log(product)
         const newCart =[...cart, product];
         setCart(newCart);
+        addToDb(product.id);
     };
     return (
         <div className="shop-container">
--- src/utilities/fakedb.js
+++ src/utilities/fakedb.js
@@ -19,7 +19,16 @@ const addToDb = id =>{
     }
     localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart));
 }
+const getStoreCart=()=>{
+    let shoppingCart = {};
 
+    //get the shopping cart from local storage
+    const storedCart = localStorage.getItem('shopping-cart');
+    if(storedCart){
+        shoppingCart = JSON.parse(storedCart);
+    }
+    return shoppingCart;
+}
 const removeFromDb = id =>{
     const storedCart = localStorage.getItem('shopping-cart');
     if(storedCart){
@@ -36,7 +45,9 @@ const deleteShoppingCart = () =>{
 }
 
 export {
-    addToDb, 
+    addToDb,
+    getStoreCart,
     removeFromDb,
     deleteShoppingCart
-}
+};
+
